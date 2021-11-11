88ba1df73db41d6adfcceceb88a49b389908cc59
canbaylan <canbaylan34@gmail.com>
2021-11-11 21:42:50 +0300

    Açıklamalar eklendi

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -1,8 +1,8 @@
 public class Singleton {
 
     // Static bir metodun içerisinden static olmayan
-    // metotlar veya değişkenler çağrılamaz. Metod static olduğundan
-    // oluşturduğumuz nesneye static keyword'unu ekleriz.
+    // metotlar veya değişkenler çağrılamaz. Metod[22] static olduğundan
+    // oluşturduğumuz değişken'e static keyword'unu ekleriz.
 
     private static Singleton singleton;
 
@@ -10,27 +10,26 @@ public class Singleton {
     {
         // Yapıcı metodu private yaparak nesneyi dışarıya kapattık.
         /// Bundan sonra başka sınıflar bu sınıfı new'leyemez.
-        /// New'leyememesini demek nesne oluşturamaması demek.
-        /// Sınıfımızın içinde nesneyi tanımlayıp metodla dışarıya açacağız.
+        /// New'leyememesi demek nesne oluşturamaması demek.
+        /// Sınıfımızın içinde nesneyi tanımlayıp[7] metodla[22] dışarıya açacağız.
 
         System.out.println("Nesne olusturuldu.");
-
     }
 
     // Bir nesneyi new'lemeden(Yapıcı metod private olduğundan) o nesneyi
-    // kullanmak için static tanımlaması yapıyoruz.
+    // kullanmak için metotda static tanımlaması yapıyoruz.
 
     public static Singleton getSingleton()
     {
         // Multithread ortamlarda aynı anda 2 çağrı yapılırsa
         // kontrollerimizde 2'side null olacak ve 2 nesne oluşturulacak.
         // synchronized anahtar sözcüğü ile thread safe'i sağlamış oluyoruz.
-        // Singleton.class tanımlaması, belirttiğimiz sınıf adını
-        // temsil eden sınıf nesnesi döndürür.
         // Thread'ler "synchronized" anahtar kelimesini gördükten sonra
         // birbirlerini beklerler. Dolayısıyla kodumuzun olabildiğince az
         // "synchronized" ile karşılaşması gerekir. Bunun için bir kontrol
-        // daha yapıyoruz.(Double Checked Loading)
+        // daha yapıyoruz.(Double Checked Loading)[34]
+        // Singleton.class tanımlaması, belirttiğimiz sınıf adını
+        // temsil eden sınıf nesnesi döndürür.
 
         if(singleton == null)
         {
