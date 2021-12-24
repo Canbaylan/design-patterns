84b49ab0c63c5028d26b47b91704824cfaab3007
Can Baylan <69698537+Canbaylan@users.noreply.github.com>
2021-12-24 16:35:22 +0300

    Add files via upload

---
+++ Builder/src/BmwSiparisBuilder.java
@@ -0,0 +1,12 @@
+public class BmwSiparisBuilder extends SiparisBuilder{
+    public BmwSiparisBuilder(){}
+    @Override
+    public void setMarka(String marka) {
+        getAraba().setMarka(new Marka(marka));
+    }
+
+    @Override
+    public void setModel(String model) {
+        getAraba().setModel(new Model(model));
+    }
+}
